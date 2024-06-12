396c368e5a94aa764c96105fbb1d8f236afc9298
saurabh <saurabhkr3122001@gmail.com>
2024-06-13 04:07:26 +0530

    setup cross-env, adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+dotenv.config();
 
 
 const app=express();
 
+//morgan
+if(process.env.NODE_ENV!== "production"){
+    app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//parson json request url
+app.use(express.json());
+
+
+//parson json request body
+app.use(express.urlencoded({extended: true}));
+
+
+//sanitise request 
+app.use(mongoSanitize());
+
+//enable cookie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(fileUpload({
+    useTempFiles: true,
+})
+);
+
+//cors
+app.use(cors());
+
 app.post('/test',(req,res)=>{
-    res.send("hello from server");
+    res.send(req.body);
 })
 
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import app from "./app.js";
-import dotenv from "dotenv";
 
 
-dotenv.config();
 
 
 const PORT= process.env.PORT || 8000;
-
+console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
     console.log(`server is listening at ${PORT}....`);
 });
