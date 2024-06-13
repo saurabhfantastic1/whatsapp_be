4ca7c339e5877a923032a6ac1fdf5b5fb0cdf29a
saurabh <saurabhkr3122001@gmail.com>
2024-06-13 11:01:53 +0530

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import app from "./app.js";
-
+import  Logger from "./configs/logger.config.js";
 
 
 
 const PORT= process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
-    console.log(`server is listening at ${PORT}....`);
+    Logger.info(`server is listening at ${PORT}....`);
 });
 
